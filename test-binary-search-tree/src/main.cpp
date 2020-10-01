4b491462de17a38b62672b1ee9df20871def736d
Karl Norden <knorden@github.com>
2020-10-01 09:43:38 -0700

    Changed some If controlflows to ternary expressions for fun

---
+++ test-binary-search-tree/src/main.cpp
@@ -0,0 +1,41 @@
+/****************************************************************************
+ **	NAME: CHALRES NORDEN, CLASS: CptS-223
+ ** ASSIGNMENT: MA2, Binary Search Tree and C++11+
+ ****************************************************************************/
+/****************************************************************************
+ * MOVE CONSTRUCTOR:
+ * A Move Constructor constructs a new data unit by assigning the resource
+ * of a passed-in data to it, while also releasing the old unit from the
+ * system.
+ *
+ * MOVE ASSIGNMENT OPERATOR:
+ * A Move Assignment Operator transfer resource from a right-value data unit
+ * to an existing left-value data unit.
+ *
+ ****************************************************************************/
+/****************************************************************************
+ * BST::empty(): worst case: O(1).
+ * It's a single operation returning the root node.
+ * BST::add(): worst case: O(N).
+ * This is when we have to append a leaf to continuous tree with one trunk
+ * and no leaves.
+ * BST::makeEmpty(): worst case: O(N).
+ * We must traverse through all nodes to delete the tree.
+ * BST::printLevelOrder(): worst case: O(N).
+ * We must traverse through all nodes to print their values.
+ * BST::cloneTree(): worst case: O(N).
+ * We must traverse through all node to clone the tree.
+ */
+// #include "testBST.h"
+#include "testMyTemplate.h"
+
+int main() {
+  std::string tSTRING = "THIS IS A TEST STRING\n";
+  std::cout << tSTRING;
+
+  // testBST();
+
+  testMyTemplate();
+
+  return 0;
+}
---
+++ test-binary-search-tree/src/testMyTemplate.cpp
@@ -0,0 +1,72 @@
+#include "testMyTemplate.h"
+
+void testMyTemplate() {
+  BinST<int> tTree{1, 2, 3, 4, 5, 6};
+  // Turn on debugging mode:
+  tTree.debug_on();
+  std::cout << "tTree: ";
+  tTree.print();
+
+  std::cout << "\n\nTest #1: deep copy constructor:\n";
+  BinST<int> deepCopiedTree(tTree);
+  std::cout << "BinST<int> deepCopiedTree(tTree): ";
+  deepCopiedTree.print();
+
+  std::cout << "\n\nTest #2: deep copy assignment operator:\n";
+  BinST<int>* tAsm = nullptr;
+  tAsm = new BinST<int>;
+  std::cout << "tAsm before copy assigment: ";
+  tAsm->print();
+  std::cout << std::endl;
+  *tAsm = deepCopiedTree;
+  std::cout << "tAsm after copy assignment: ";
+  tAsm->print();
+
+  std::cout << "\n\nTest #3: tTree destructed:\n";
+  std::cout << "tTree before destructor is called: ";
+  tTree.print();
+  std::cout << "\ntTree.~BinST() is invoked.\n";
+  tTree.~BinST();
+  std::cout << "call tTree.printInOrder() to check values of tTree after "
+               "destruction: ";
+  tTree.print();
+  std::cout << "\ntTree destructed successfully.\n";
+
+  std::cout << "\n\nTest #4: move constructor:\n";
+  std::cout << "Tree A before moving: ";
+  deepCopiedTree.print();
+  std::cout << "\nTree A's height before moving: "
+            << deepCopiedTree.getHeight();
+  BinST<int> tTree_new = std::move(deepCopiedTree);
+  std::cout << "\nTree A after moving: ";
+  deepCopiedTree.print();
+  std::cout << "\nTree A's height after moving: " << deepCopiedTree.getHeight();
+  std::cout << "\nNewly constructed tree B after moving: ";
+  tTree_new.print();
+  std::cout << "\nNewly constructor tree B's height after moving: "
+            << tTree_new.getHeight();
+
+  std::cout << "\n\nTest #5: move assignment constructor:\n";
+  BinST<int> tA{1, 3, 5, 6}, tB{2, 5, 4, 8, 10};
+  std::cout << "Before moving:\n";
+  std::cout << "Tree A: ";
+  tA.print();
+  std::cout << std::endl;
+  std::cout << "Tree B: ";
+  tB.print();
+  tA = std::move(tB);
+  std::cout << std::endl;
+  std::cout << "After moving:\n";
+  std::cout << "Tree A: ";
+  tA.print();
+  std::cout << std::endl;
+  std::cout << "Tree B: ";
+  tB.print();
+  std::cout << std::endl;
+
+  std::cout << "\n\nTest #6: BST::printLevelOrderHelper():\n";
+  BinST<int> tC{3, 5, 4, 1, 6, 8, 13, 25};
+  tC.print_LevelOrder();
+
+  std::cout << "\n\n\nEnd of Test.\nAll trees destroyed." << std::endl;
+}
